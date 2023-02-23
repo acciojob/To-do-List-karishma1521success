2463eca03ad6dc565c914c244a4f39d3fe38fd75
karishma1521success <105378376+karishma1521success@users.noreply.github.com>
2023-02-23 22:21:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
 
+
+let button = document.getElementById('addTodoBtn');
+let ol = document.getElementById('todoList');
+
+function addTodo(event){
+    event.preventDefault();
+    let inputValue = document.getElementById('newTodoInput').value;
+    if(inputValue == ""){
+        return;
+    }
+    //1.create li element
+    let li = document.createElement('li');
+    //2.set content of li element
+    li.textContent = inputValue;
+    //3. now add li to ol parent element
+    ol.appendChild(li);
+    document.getElementById('newTodoInput').value = "";   
+}
+
+button.addEventListener('click',addTodo);
+
+
+
